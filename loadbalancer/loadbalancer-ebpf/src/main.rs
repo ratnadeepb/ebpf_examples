b9f66eedc14eb98f9b3081b3ef37fc705b655ae5
Ratnadeep Bhattacharya <ratnadeepb.bhattacharya1983@gmail.com>
2021-10-19 09:53:55 -0600

    adding ebpf aya rust

---
+++ loadbalancer/loadbalancer-ebpf/src/main.rs
@@ -0,0 +1,62 @@
+#![no_std]
+#![no_main]
+
+mod bindings;
+
+use aya_bpf::{
+    bindings::xdp_action, macros::map, macros::xdp, maps::PerfEventArray, programs::XdpContext,
+};
+use bindings::{ethhdr, iphdr};
+use core::mem;
+use loadbalancer_common::PacketLog;
+use memoffset::offset_of;
+
+const ETH_P_IP: u16 = 0x0800;
+const ETH_HDR_LEN: usize = mem::size_of::<ethhdr>();
+
+#[map(name = "EVENTS")]
+static mut EVENTS: PerfEventArray<PacketLog> =
+    PerfEventArray::<PacketLog>::with_max_entries(1024, 0);
+
+// #[xdp(name = "loadbalancer")]
+#[xdp]
+pub fn loadbalancer(ctx: XdpContext) -> u32 {
+    match unsafe { try_loadbalancer(ctx) } {
+        Ok(ret) => ret,
+        Err(_) => xdp_action::XDP_ABORTED,
+    }
+}
+
+#[inline(always)]
+unsafe fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
+    let start = ctx.data();
+    let end = ctx.data_end();
+    let len = mem::size_of::<T>();
+
+    if start + len > end {
+        return Err(());
+    }
+
+    Ok((start + offset) as *const T)
+}
+
+unsafe fn try_loadbalancer(ctx: XdpContext) -> Result<u32, ()> {
+    let h_proto = u16::from_be(unsafe { *ptr_at(&ctx, offset_of!(ethhdr, h_proto))? });
+    if h_proto != ETH_P_IP {
+        return Ok(xdp_action::XDP_PASS);
+    }
+    let source = u32::from_be(unsafe { *ptr_at(&ctx, ETH_HDR_LEN + offset_of!(iphdr, saddr))? });
+    let log_entry = PacketLog {
+        ipv4_address: source,
+        action: xdp_action::XDP_PASS,
+    };
+    unsafe {
+        EVENTS.output(&ctx, &log_entry, 0);
+    }
+    Ok(xdp_action::XDP_PASS)
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    unsafe { core::hint::unreachable_unchecked() }
+}
---
+++ loadbalancer/loadbalancer/src/main.rs
@@ -0,0 +1,100 @@
+use aya::{
+    maps::perf::AsyncPerfEventArray,
+    programs::{Xdp, XdpFlags},
+    util::online_cpus,
+    Bpf,
+};
+use bytes::BytesMut;
+use loadbalancer_common::PacketLog;
+use std::{
+    convert::{TryFrom, TryInto},
+    env,
+    fs,
+    net,
+    // sync::atomic::{AtomicBool, Ordering},
+    // sync::Arc,
+    // thread,
+    // time::Duration,
+};
+use structopt::StructOpt;
+use tokio::{signal, task};
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let path = match env::args().nth(1) {
+        Some(iface) => iface,
+        None => panic!("not path provided"),
+    };
+    let iface = match env::args().nth(2) {
+        Some(iface) => iface,
+        None => "eth0".to_string(),
+    };
+
+    let data = fs::read(path)?;
+    let mut bpf = Bpf::load(&data)?;
+
+    let probe: &mut Xdp = bpf.program_mut("xdp")?.try_into()?;
+    probe.load()?;
+    probe.attach(&iface, XdpFlags::default())?;
+
+    let mut perf_array = AsyncPerfEventArray::try_from(bpf.map_mut("EVENTS")?)?;
+
+    for cpu_id in online_cpus()? {
+        let mut buf = perf_array.open(cpu_id, None)?;
+
+        task::spawn(async move {
+            let mut buffers = (0..10)
+                .map(|_| BytesMut::with_capacity(1024))
+                .collect::<Vec<_>>();
+
+            loop {
+                let events = buf.read_events(&mut buffers).await.unwrap();
+                for i in 0..events.read {
+                    let buf = &mut buffers[i];
+                    let ptr = buf.as_ptr() as *const PacketLog;
+                    let data = unsafe { ptr.read_unaligned() };
+                    let src_addr = net::Ipv4Addr::from(data.ipv4_address);
+                    println!("LOG: SRC {}, ACTION {}", src_addr, data.action);
+                }
+            }
+        });
+    }
+
+    // if let Err(e) = try_main() {
+    //     eprintln!("error: {:#}", e);
+    // }
+    signal::ctrl_c().await.expect("failed to listen for event");
+    Ok::<_, anyhow::Error>(())
+}
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    #[structopt(short, long)]
+    path: String,
+    #[structopt(short, long, default_value = "eth0")]
+    iface: String,
+}
+
+// fn try_main() -> Result<(), anyhow::Error> {
+//     let opt = Opt::from_args();
+//     let mut bpf = Bpf::load_file(&opt.path)?;
+//     let program: &mut Xdp = bpf.program_mut("loadbalancer")?.try_into()?;
+//     program.load()?;
+//     program.attach(&opt.iface, XdpFlags::default())?;
+
+//     let running = Arc::new(AtomicBool::new(true));
+//     let r = running.clone();
+
+//     ctrlc::set_handler(move || {
+//         r.store(false, Ordering::SeqCst);
+//     })
+//     .expect("Error setting Ctrl-C handler");
+
+//     println!("Waiting for Ctrl-C...");
+//     while running.load(Ordering::SeqCst) {
+//         thread::sleep(Duration::from_millis(500))
+//     }
+//     println!("Exiting...");
+
+//     Ok(())
+// }
